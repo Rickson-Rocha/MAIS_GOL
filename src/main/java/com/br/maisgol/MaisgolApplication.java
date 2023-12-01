07093449c044419106ba0623f33a1df54c202c91
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-01 20:30:57 -0300

    first commit

---
+++ src/main/java/com/br/maisgol/MaisgolApplication.java
@@ -0,0 +1,13 @@
+package com.br.maisgol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisgolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisgolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/maisgol/MaisgolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.maisgol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisgolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
