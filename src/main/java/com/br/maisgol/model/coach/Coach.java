bf59f2c42c207efc38eac6359ac530edc5bce41b
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-02 18:48:57 -0300

    fix: add Digits notation in cpf and phone attr

--- src/main/java/com/br/maisgol/model/coach/Coach.java
+++ src/main/java/com/br/maisgol/model/coach/Coach.java
@@ -1,15 +1,21 @@
 package com.br.maisgol.model.coach;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.br.maisgol.model.enums.Status;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -24,9 +30,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Coach {
+public class Coach implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_coach")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Long id;
 
     @NotNull
@@ -34,18 +42,25 @@ public class Coach {
     private String name;
 
     @NotNull
-    @Column(name = "cpf_coach")
+    @Column(name = "cpf_coach",unique = true)
+    @Digits(integer = 11, fraction = 0, message = "O CPF deve conter apenas dígitos")
     private String cpf;
 
     @Column(name = "phone_coach")
     @NotNull
+    @Digits(integer = 12, fraction = 0, message = "O Número de telefone deve conter apenas dígitos")
     private String phone;
 
     @Column(name="status_coach")
     @NotNull
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Column(name = "birth_date_coach")
     @NotNull
     private LocalDate birthDay;
+
+    @Lob
+    @Column(name = "photo_coach")
+    private byte[] photo;
 }
