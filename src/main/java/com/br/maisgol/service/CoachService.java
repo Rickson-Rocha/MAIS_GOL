8c165279169533a73306bc2049a1ddc0fb74eaf9
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-02 18:45:07 -0300

    feature: add filter methods

--- src/main/java/com/br/maisgol/service/CoachService.java
+++ src/main/java/com/br/maisgol/service/CoachService.java
@@ -1,12 +1,15 @@
 package com.br.maisgol.service;
 
+import java.io.IOException;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.br.maisgol.model.coach.Coach;
+import com.br.maisgol.model.enums.Status;
 
-@Service
+
 public interface CoachService {
     
     Coach findById(Long id);
@@ -15,7 +18,17 @@ public interface CoachService {
 
     Coach createCoach(Coach coach);
 
+    void uploadCoachPhoto(Long coachId, MultipartFile photoFile) throws IOException;
+
     Coach updateCoach(Coach coach);
 
     void delete(Long id);
+
+    Page<Coach> findActiveCoaches(Pageable pageable);
+
+    Page<Coach> findInactiveCoaches(Pageable pageable);
+
+    Page<Coach> findVacationCoaches(Pageable pageable);
+
+    Page<Coach> findCoachesByCriteria(String name, Status status,Pageable pageable);
 }
