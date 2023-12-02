474cc42f40a040b694ec54d9b467963597d5f97c
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-02 18:51:09 -0300

    feature: findByStatys and findByNameAndStatus

--- src/main/java/com/br/maisgol/repository/CoachRepository.java
+++ src/main/java/com/br/maisgol/repository/CoachRepository.java
@@ -1,11 +1,20 @@
 package com.br.maisgol.repository;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.br.maisgol.model.coach.Coach;
+import com.br.maisgol.model.enums.Status;
 
 @Repository
 public interface CoachRepository extends JpaRepository<Coach,Long>{
+
+    Page<Coach> findByStatus(Status active, Pageable pageable);
+
+    Page<Coach> findByNameAndStatus(String name, Status status,Pageable pageable);
     
 }
