94acb7758e396f08fc6922b8027129deb7200c1c
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-18 06:59:20 -0300

    config: add exceptions

---
+++ src/main/java/com/br/maisgol/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.br.maisgol.exception;
+
+public class ConflictException extends Exception {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/br/maisgol/exception/ScheduleConflictException.java
@@ -0,0 +1,7 @@
+package com.br.maisgol.exception;
+
+public class ScheduleConflictException  extends Exception {
+    public ScheduleConflictException(String message) {
+        super(message);
+    }
+}
