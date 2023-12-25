38cb31507e94e7a5f1b7f2dc992f8de4a7f53fa2
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-25 17:01:50 -0300

    config: add cors configuration

---
+++ src/main/java/com/br/maisgol/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.br.maisgol.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    
+    public void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
