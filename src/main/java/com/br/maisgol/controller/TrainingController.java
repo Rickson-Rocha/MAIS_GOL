68c16c6ace5ec4dc77c72bd6999e228e0eb7891e
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-25 17:09:08 -0300

    add: PostMapping for create training

--- src/main/java/com/br/maisgol/controller/TrainingController.java
+++ src/main/java/com/br/maisgol/controller/TrainingController.java
@@ -1,54 +1,29 @@
-// package com.br.maisgol.controller;
+package com.br.maisgol.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.br.maisgol.model.coach.Coach;
-// import com.br.maisgol.model.field.Field;
-// import com.br.maisgol.model.group.Group;
-// import com.br.maisgol.model.schedule.Schedule;
-// import com.br.maisgol.model.training.Training;
-// import com.br.maisgol.service.TrainingService;
-// import com.br.maisgol.service.TrainingServiceImpl;
+import com.br.maisgol.DTO.TrainingRequestDTO;
+import com.br.maisgol.service.TrainingService;
 
-// import jakarta.transaction.Transactional;
-// import jakarta.validation.Valid;
+@RestController
+@RequestMapping("/trainings")
+public class TrainingController {
 
-// import java.time.DayOfWeek;
-// import java.time.LocalTime;
-// import java.util.ArrayList;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/trainings")
-// public class TrainingController {
-   
-//     @Autowired
-//     private TrainingService trainingService;
-
-// @PostMapping("/create")
-// public ResponseEntity<?> createTraining(@RequestBody List<Training> trainingList) {
-//      try {
-//         Training training;
-//         List<Schedule> selectedSchedules = training.getSchedules();
-//         Group group = training.getGroup();
-//         Coach coach = training.getCoach();
-//         Field field = training.getField();
-
-//         // Seu código para validar a disponibilidade e criar o treinamento...
-
-//         Training createdTraining = trainingService.createTraining(group, coach, field, selectedSchedules);
-//         return new ResponseEntity<>(createdTraining, HttpStatus.CREATED);
-//     } catch (RuntimeException e) {
-//         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-//     }
-// }
-
-   
-// }
+    @Autowired
+    private TrainingService trainingService;
+    @PostMapping("/create")
+    public ResponseEntity<String> createTraining(@RequestBody TrainingRequestDTO requestDTO) {
+        try {
+            trainingService.createTraining(requestDTO.getGroupId(), requestDTO.getCoachId(), requestDTO.getSchedules());
+            return ResponseEntity.ok("Training created successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create training: " + e.getMessage());
+        }
+    }
+}
