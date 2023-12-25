3cf8f44e36cd66224966206d6c8ff6b2c9409290
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-25 17:18:36 -0300

    fix:coach attribute to ManyToOne notation

--- src/main/java/com/br/maisgol/model/schedule/Schedule.java
+++ src/main/java/com/br/maisgol/model/schedule/Schedule.java
@@ -2,22 +2,20 @@ package com.br.maisgol.model.schedule;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
-import java.util.List;
 
+import com.br.maisgol.model.coach.Coach;
 import com.br.maisgol.model.training.Training;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -34,10 +32,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Schedule {
-
-    public Schedule(LocalTime startTime2, LocalTime endTime2) {
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_schedule")
@@ -61,5 +55,8 @@ public class Schedule {
     @JoinColumn(name = "training_id")
     private Training training;
 
+    @ManyToOne // Relacionamento Many-to-One com Coach
+    @JoinColumn(name = "coach_id") // Mapeamento da chave estrangeira
+    private Coach coach;
     
 }
