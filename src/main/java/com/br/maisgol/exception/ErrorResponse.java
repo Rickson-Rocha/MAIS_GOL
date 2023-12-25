5d8536807589eeacf837cd62a9bc652581ee5bc5
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-25 17:13:02 -0300

    goal_net: add global exceptions

---
+++ src/main/java/com/br/maisgol/exception/ErrorResponse.java
@@ -0,0 +1,36 @@
+package com.br.maisgol.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    private static class ValidationError {
+        private final String field;
+        private final String message;
+    }
+     public void addValidationError(String field, String message) {
+        if (Objects.isNull(errors)) {
+            this.errors = new ArrayList<>();
+        }
+        this.errors.add(new ValidationError(field, message));
+    }
+}
