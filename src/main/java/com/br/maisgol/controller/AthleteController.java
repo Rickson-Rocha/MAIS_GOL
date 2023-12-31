fd28e8eee279fc700bdcb764ead52982832406ac
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-31 14:47:15 -0300

    add: athlete controller

--- src/main/java/com/br/maisgol/controller/AthleteController.java
+++ src/main/java/com/br/maisgol/controller/AthleteController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
