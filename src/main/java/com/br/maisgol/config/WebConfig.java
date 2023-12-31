819a34c2e0562ec633aae090d888a783188478fc
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-12-30 23:06:02 -0300

    fix: resolve bug in addCorsMappings

--- src/main/java/com/br/maisgol/config/WebConfig.java
+++ src/main/java/com/br/maisgol/config/WebConfig.java
@@ -8,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
-    
-    public void addCorsMapping(CorsRegistry registry){
-        registry.addMapping("/**");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir requisições de qualquer origem. Você pode especificar as origens permitidas conforme necessário.
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Headers permitidos
     }
 }
