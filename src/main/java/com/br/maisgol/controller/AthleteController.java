bb63fe9e1c18cd2f66b64c3ae45fe7c5a5f1e87a
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-01-01 17:31:20 -0300

    add:adds crud and filtering methods

--- src/main/java/com/br/maisgol/controller/AthleteController.java
+++ src/main/java/com/br/maisgol/controller/AthleteController.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.br.maisgol.model.athletes.Athletes;
 import com.br.maisgol.service.AthletesService;
 import com.br.maisgol.service.exceptions.ObjectNotFoundException;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("athlete")
@@ -50,6 +55,37 @@ public class AthleteController {
    }
     
     }
-   
+   @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Athletes> update(@PathVariable Long id,@RequestBody @Valid Athletes athletes) {
+        athletes.setId(id);
+        Athletes localAthlete = athletesService.updateAthletes(athletes);
+
+        return ResponseEntity.ok(localAthlete);
+    }
+
+    @GetMapping("/active")
+    public ResponseEntity<Page<Athletes>> getActiveAthletes(
+            @PageableDefault(size = 10) Pageable pageable // Define o tamanho da página como 10 por padrão
+    ) {
+        Page<Athletes> activeAthletesPage = athletesService.findActiveAthletes(pageable);
+        return ResponseEntity.ok(activeAthletesPage);
+    }
+
+    @GetMapping("/inactive")
+    public ResponseEntity<Page<Athletes>> getInactiveAthletes(
+            @PageableDefault(size = 10) Pageable pageable // Define o tamanho da página como 10 por padrão
+    ) {
+        Page<Athletes> activeAthletesPage = athletesService.findInactiveAthletes(pageable);
+        return ResponseEntity.ok(activeAthletesPage);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        athletesService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
